116b700259c3a7f846c1c31bc6077a980cdc7aa8
Vadim <sekret.vad@gmail.com>
2024-02-13 15:54:51 +0300

    + data add-books

--- add-books.js
+++ add-books.js
@@ -2,12 +2,20 @@ import crc32 from 'crc-32';
 
 const getIndex = (book) => Math.abs(crc32.str(key)) % 1000;
 
-const set = (map, book, value) => {
-    const index = getIndex(key);
-    if (map[index] && hasCollision(map, key)) {
+const hasCollision = (bookshelf, book) => {
+  const index = getIndex(book)
+  const [currentKey] = map[index]
+  return currentKey !== book
+}
+
+const setF = (bookshelf, book, value) => {
+    const index = getIndex(book);
+    if (bookshelf[index] && hasCollision(bookshelf, book)) {
       return false;
     }
-    map[index] = [key, value];
+    bookshelf[index] = [book, value];
   
     return true;
   };
+
+export default setF;
